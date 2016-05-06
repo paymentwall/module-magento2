c84570dbbb8f23244e46a4e252f7de9cbd42c86e
Doanh Tien Hoang <tien.doanh@pw.loc>
2016-05-06 10:58:52 +0700

    Update Invoice, Transaction

---
+++ src/app/code/Paymentwall/Paymentwall/Model/Adminhtml/Source/PaymentAction.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace Paymentwall\Paymentwall\Model\Adminhtml\Source;
+ 
+/**
+ * Class PaymentAction
+ */
+class PaymentAction implements \Magento\Framework\Option\ArrayInterface
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function toOptionArray()
+    {
+        return [
+            [
+                'value' => AbstractMethod::ACTION_AUTHORIZE,
+                'label' => __('Authorize')
+            ]
+        ];
+    }
+}
