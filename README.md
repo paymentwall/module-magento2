30996d9c11592c74d4f457ca76b2a3bf8ee8305e
Dzung Tran <hello@iamdzung.com>
2017-02-27 19:34:45 +0700

    Fix bugs for Magento 2.1

--- README.md
+++ README.md
@@ -1,13 +1,1 @@
-#About Magento 2 module
-Paymentwall module for Magento 2.
-
-
-###Versions
-* Tested on Magento 2.0.2
-* PHP 5.3 or later
-
-#Installation
-To install Paymentwall Magento 2 module, please follow the [instructions](https://www.paymentwall.com/en/documentation/Magento-2/4013).
-
-After cloning the repository don't forget to install Paymentwall PHP API library (**required**):
-`git submodule init` and then `git submodule update`
+### Paymentwall module for Magento 2
--- src/view/frontend/web/js/view/payment/method-renderer/paymentwall.js
+++ src/view/frontend/web/js/view/payment/method-renderer/paymentwall.js
@@ -1,8 +1,10 @@
 define(
     [
-        'Magento_Checkout/js/view/payment/default'
+        'Magento_Checkout/js/view/payment/default',
+        'Magento_Checkout/js/checkout-data',
+        'Magento_Customer/js/customer-data'
     ],
-    function (Component) {
+    function (Component,checkout,storage) {
         'use strict';
 
         return Component.extend({
@@ -35,11 +37,28 @@ define(
             },
 
             getValidatedEmailValue: function () {
-                return JSON.parse(localStorage['mage-cache-storage'])['checkout-data']['validatedEmailValue'];
+                return checkout.getValidatedEmailValue();
+            },
+
+            getBillingValue: function () {
+                return JSON.stringify(checkout.getBillingAddressFromData());
             },
 
             placeOrder: function () {
-                jQuery("#frmPaymentwall").submit();
+                var cart = {
+                    'data_id': null,
+                    'extra_actions' : null,
+                    'isGuestCheckoutAllowed' : null,
+                    'items' : [],
+                    'possible_onepage_checkout' : null,
+                    'subtotal' : null,
+                    'subtotal_excl_tax' : null,
+                    'subtotal_incl_tax' : null,
+                    'summary_count': null,
+                    'website_id': null
+                };
+                storage.set('cart',cart);
+                document.getElementById("frmPaymentwall").submit();
             },
         });
     }
