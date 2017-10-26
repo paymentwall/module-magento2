4b160cb29de0e14b2712dd935514d2c68a0ed482
mason <mason@paymentwall.com>
2017-10-26 16:23:39 +0700

    Enable debug mode for pingback

--- Model/Pingback.php
+++ Model/Pingback.php
@@ -60,7 +60,7 @@ class Pingback
         $realIp =  $objRemoteAddress->getRemoteAddress();
 
         $pingback = new \Paymentwall_Pingback($getData, $realIp);
-        if ($pingback->validate()) {
+        if ($pingback->validate(true)) {
             if ($method == Paymentwall::PAYMENT_METHOD_CODE) {
                 $result = $this->pwLocalPingback($orderModel, $pingback);
             } else {
