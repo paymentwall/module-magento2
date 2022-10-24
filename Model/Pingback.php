4761b54c47a0a7a54577b0fe16e7de9e7cbe3f80
Peter Nguyen <peter@outcubator.com>
2022-10-24 14:10:44 +0700

    [PLAT-326] Add Ref ID to order view

--- Model/Pingback.php
+++ Model/Pingback.php
@@ -503,8 +503,8 @@ class Pingback
                     ->addObject($invoice->getOrder());
                 $transactionSave->save();
 
-                $order->addStatusHistoryComment(__('Created invoice #%1.', $invoice->getId()))
-                    ->setIsCustomerNotified(true)->save();
+                $order->addStatusHistoryComment(__('Created invoice #%1.', $invoice->getId() . ', Paymentwall transaction Reference ID: ' . $pingback->getReferenceId()))->setIsCustomerNotified(true)->save();
+
                 $this->createTransaction($order, $pingback->getReferenceId());
             }
         } catch (\Exception $e) {
