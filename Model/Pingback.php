231ee189e340bd4f234ca086c71f498c253891b8
Peter Nguyen <peter@outcubator.com>
2022-10-27 15:59:08 +0700

    [PLAT-326] refactor code

--- Model/Pingback.php
+++ Model/Pingback.php
@@ -505,7 +505,7 @@ class Pingback
                     ->addObject($invoice->getOrder());
                 $transactionSave->save();
 
-                $order->addStatusHistoryComment(__('Created invoice #%1.', $invoice->getId() . ', Paymentwall transaction reference ID: ' . $pingback->getReferenceId()))->setIsCustomerNotified(true)->save();
+                $order->addStatusHistoryComment(__('Created invoice #%1. Paymentwall transaction reference ID: %2', [$invoice->getId(), $pingback->getReferenceId()]))->setIsCustomerNotified(true)->save();
 
                 $this->createTransaction($order, $pingback->getReferenceId());
             }
